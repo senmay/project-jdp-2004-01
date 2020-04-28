600a68087a1608b793a22ab23d6f9c96ef1b8ef3
Karolina-rm <karolina.ra.kr@gmail.com>
2020-04-28 21:26:02 +0200

    task JDP200401-1, endpoints implementation

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,38 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
 public class ProductController {
+
+    @RequestMapping(method = RequestMethod.POST, value = "createProduct")
+    public void createProduct(ProductDto productDto) {
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getProduct")
+    public ProductDto getProduct(Long productId) {
+        return new ProductDto(1L,"test name", "test description");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value ="getProducts" )
+    public List<ProductDto> getProducts() {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateProduct")
+    public ProductDto updateProduct(ProductDto productDto) {
+        return new ProductDto(1L, "Edited test name", "test description");
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
+    public void deleteProduct(Long productId) {
+
+    }
+
 }
---
+++ src/main/java/com/kodilla/ecommercee/ProductDto.java
@@ -0,0 +1,12 @@
+package com.kodilla.ecommercee;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ProductDto {
+    private Long id;
+    private String name;
+    private String description;
+}
