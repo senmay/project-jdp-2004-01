e2d53db86c574f65bfdf1199cfff8388e9eb5ea0
Klaudia <klaudia@MacBook-Pro-Klaudia.local>
2020-04-29 17:35:19 +0200

    task JDP200401-3, endpoints implementation

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -1,4 +1,35 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/v1/cart")
 public class CartController {
+
+    @RequestMapping(method = RequestMethod.POST, value = "createCart", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createCart(@RequestBody CartDto cartDto) {
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getProduct")
+    public ProductDto getProduct(@RequestParam Long productId) {
+        return null;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "addProduct")
+    public void addProduct(@RequestBody ProductDto productDto){
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
+    public void deleteProduct(@RequestParam Long productId) {
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createOrder", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createOrder(@RequestBody OrderDto orderDto) {
+    }
+
+
+
 }
