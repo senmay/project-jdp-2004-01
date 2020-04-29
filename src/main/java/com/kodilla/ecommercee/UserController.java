6680fbb7bc9d885940e75225f1c1a5c5908c0717
daniellsoon <daniel.adamiec@hotmail.com>
2020-04-29 22:29:29 +0200

    User Controller added

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,4 +1,26 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @RequestMapping(method = RequestMethod.POST, value = "createUser", consumes = APPLICATION_JSON_VALUE)
+    public void createUser(@RequestBody UserDto userDto) {
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "banUser")
+    public UserDto banUser(@RequestParam Long userId) {
+        return new UserDto(1L, "BannedUser", false, null);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "keyGenerator" )
+    public UserDto apiKeyGenerator(@RequestBody UserDto userDto) {
+        return new UserDto(2L, "KeyAddedToUser", true, "123asd");
+    }
 }
+
+
