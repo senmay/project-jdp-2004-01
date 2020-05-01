5aacfa8f8ca37d7a4d18b2ae254875d8b5d103e2
Klaudia <klaudia@MacBook-Pro-Klaudia.local>
2020-05-01 20:01:24 +0200

    v1.4 method structure changes, update gradle

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -1,4 +1,41 @@
 package com.kodilla.ecommercee.controller;
 
+
+import com.kodilla.ecommercee.domain.CartDto;
+import com.kodilla.ecommercee.domain.OrderDto;
+import com.kodilla.ecommercee.domain.ProductDto;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/v1/cart")
 public class CartController {
+
+    @RequestMapping(method = RequestMethod.POST, value = "createCart", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createCart(@RequestBody CartDto cartDto) {
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getProducts")
+    public List<ProductDto> getProducts() {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "addProduct")
+    public void addProduct(@RequestParam Long productId){
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
+    public void deleteProduct(@RequestParam Long productId) {
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createOrder", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createOrder(@RequestBody OrderDto orderDto) {
+    }
+
+
+
 }
