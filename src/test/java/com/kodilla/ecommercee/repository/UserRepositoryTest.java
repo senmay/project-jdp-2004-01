58470d1bb5416a2e491901f90bbb4e4cbef10989
senmay <dominik.szarzynski@gmail.com>
2020-05-10 22:46:38 +0200

    adding UserRepository

---
+++ src/test/java/com/kodilla/ecommercee/repository/UserRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.kodilla.ecommercee.repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserRepositoryTest {
+
+    @Test
+    public void findAll() {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void deleteById() {
+    }
+}
