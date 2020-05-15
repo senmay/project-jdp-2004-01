96d43a6175da3d9f837d3b31b7c1a26299faac65
Klaudia <klaudia@MacBook-Pro-Klaudia.local>
2020-05-15 19:33:14 +0200

    ProductTestSuite

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,21 +1,19 @@
 package com.kodilla.ecommercee.domain;
-
-
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
 import java.util.List;
 
+
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity(name = "products")
 public class Product {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(unique = true)
@@ -25,33 +23,30 @@ public class Product {
     @Column
     @NotNull
     private String name;
-
     @Column
     @NotNull
     private String description;
 
+
+
+    //    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    //    @JoinColumn(name = "group_id")
+    //    private Group group;
     @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn
     private Group group;
 
-    @OneToMany(targetEntity = OrderItem.class,
-            mappedBy = "product",
-            cascade = CascadeType.ALL,
-            fetch = FetchType.LAZY)
-    List<OrderItem> orderItemList;
+    //    @OneToMany(targetEntity = Item.class, mappedBy = "product", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    //    @JoinColumn
+    //    List<OrderItem> orderItemList;
+    //    @OneToMany(targetEntity = CartItem.class, mappedBy = "product", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    //    @JoinColumn
+    //    List<CartItem> cartItemList;
 
-    @OneToMany(targetEntity = CartItem.class,
-            mappedBy = "product",
-            cascade = CascadeType.ALL,
-            fetch = FetchType.LAZY)
-    List<CartItem> cartItemList;
-
-    public Product(Long id, String name, String description) {
-        this.id = id;
+    public Product(String name, String description) {
         this.name = name;
         this.description = description;
-        orderItemList = new ArrayList<>();
-        cartItemList = new ArrayList<>();
+
     }
     public Product( String name, String description) {
         this.name = name;
@@ -72,5 +67,3 @@ public class Product {
         this.group = group;
     }
 }
-
-
