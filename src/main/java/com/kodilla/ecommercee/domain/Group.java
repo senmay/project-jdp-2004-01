76d3b1ae453c012c7cbbc196cdb07293ed8f2de7
senmay <dominik.szarzynski@gmail.com>
2020-05-16 15:51:40 +0200

    rebase x39

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -1,33 +1,34 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity(name = "product_groups")
+@Getter
+@NoArgsConstructor
 public class Group {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column
+    @Column(unique = true)
+    @NotNull
     private Long id;
 
+    @NotNull
     @Column
     private String groupName;
+
+
     @OneToMany(
             targetEntity = Product.class,
-            mappedBy =("group"),
-            fetch = FetchType.LAZY,
-            cascade = CascadeType.ALL
+            mappedBy ="group",
+            fetch = FetchType.EAGER,
+            cascade = CascadeType.PERSIST
     )
     private List<Product> products = new ArrayList<>();
 
