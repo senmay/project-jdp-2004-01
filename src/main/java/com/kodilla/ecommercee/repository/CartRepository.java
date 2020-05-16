f8083bc081571b51f02f6e2d5b2f60887cab4044
senmay <dominik.szarzynski@gmail.com>
2020-05-16 14:10:18 +0200

    t1

--- src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
@@ -3,14 +3,10 @@ package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.domain.Cart;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-@Transactional
-@Repository
 public interface CartRepository extends CrudRepository<Cart, Long> {
 
     @Override
