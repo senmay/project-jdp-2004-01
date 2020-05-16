80ee3d5397b187a951564d2eaf9aa8b8cd5ab096
senmay <dominik.szarzynski@gmail.com>
2020-05-16 14:10:18 +0200

    t1

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,44 +1,50 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "USERS")
 public class User {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.AUTO )
-        @Column (unique = true)
-        private Long id;
+    @Id
+    @GeneratedValue
+    @Column
+    private Long id;
 
-        @NotNull
-        private String name;
+    @NotNull
+    private String name;
 
-        @NotNull
-        private boolean isActive;
+    @NotNull
+    private boolean isActive;
 
-        @NotNull
-        private String apiKey;
-
-
-        @OneToMany(targetEntity = Order.class,
-                mappedBy = "user",
-                cascade = CascadeType.ALL,
-                fetch = FetchType.LAZY)
-        List<Order> orders;
+    @NotNull
+    private String apiKey;
 
 
-        @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-        @JoinColumn
-        Cart cart;
+    @OneToMany(targetEntity = Order.class,
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY)
+    private List<Order> orders = new ArrayList<>();
+
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn
+    Cart cart;
+
+    public User(Long id, String name, boolean isActive, String apiKey) {
+        this.name = name;
+        this.isActive = isActive;
+        this.apiKey = apiKey;
+    }
+
 }
