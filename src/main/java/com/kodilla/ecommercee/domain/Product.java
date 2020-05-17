66c58bd71fc7d0f505d137574e640848eb8e4275
Klaudia <klaudia@MacBook-Pro-Klaudia.local>
2020-05-17 21:31:35 +0200

    deleted joinColumn for CartItem and OrderItem from Product class

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,19 +1,21 @@
 package com.kodilla.ecommercee.domain;
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.List;
 
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity(name = "products")
 public class Product {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(unique = true)
@@ -23,30 +25,26 @@ public class Product {
     @Column
     @NotNull
     private String name;
+
     @Column
     @NotNull
     private String description;
 
-
-
-    //    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    //    @JoinColumn(name = "group_id")
-    //    private Group group;
     @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn
     private Group group;
 
-    //    @OneToMany(targetEntity = Item.class, mappedBy = "product", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    //    @JoinColumn
-    //    List<OrderItem> orderItemList;
-    //    @OneToMany(targetEntity = CartItem.class, mappedBy = "product", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    //    @JoinColumn
-    //    List<CartItem> cartItemList;
+    @OneToMany(targetEntity = OrderItem.class, mappedBy = "product", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    List<OrderItem> orderItemList;
+
+    @OneToMany(targetEntity = CartItem.class, mappedBy = "product", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    List<CartItem> cartItemList;
 
     public Product(String name, String description) {
         this.name = name;
         this.description = description;
-
+        orderItemList = new ArrayList<>();
+        cartItemList = new ArrayList<>();
     }
     public Product( String name, String description) {
         this.name = name;
