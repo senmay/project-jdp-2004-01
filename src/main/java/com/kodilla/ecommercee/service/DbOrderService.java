eb3fa107154431860f4d6a080f9c8483ff77b358
abialkowska <abialkowska247@gmail.com>
2020-05-24 22:34:52 +0200

    create order service

---
+++ src/main/java/com/kodilla/ecommercee/service/DbOrderService.java
@@ -0,0 +1,39 @@
+package com.kodilla.ecommercee.service;
+
+import com.kodilla.ecommercee.domain.Order;
+import com.kodilla.ecommercee.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class DbOrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Optional<Order> getById(final Long id) {
+        return orderRepository.findById(id);
+    }
+
+    public Order save(Order task) {
+        return orderRepository.save(task);
+    }
+
+
+    public boolean deleteById(final Long id) {
+        if (orderRepository.existsById(id)) {
+            orderRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
