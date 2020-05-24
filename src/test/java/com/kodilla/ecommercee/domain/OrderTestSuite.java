c5f69e0cc38ba91d0fac79e02b295cd4951fb9ef
jagiello430 <jagiello430@o2.pl>
2020-05-24 18:09:19 +0000

    test v2

--- src/test/java/com/kodilla/ecommercee/domain/OrderTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/OrderTestSuite.java
@@ -1,13 +1,11 @@
 package com.kodilla.ecommercee.domain;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.kodilla.ecommercee.repository.*;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.assertj.AssertableApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
